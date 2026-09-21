7d64d21a836a38fd3450d3affa12484354e089f5
Tris <hbabnd@gmail.com>
2026-09-22 02:20:44 +0900

    chore - add location dependency and CI

--- Package.swift
+++ Package.swift
@@ -1,23 +1,52 @@
 // swift-tools-version: 5.9
+
 import PackageDescription
 
 let package = Package(
     name: "WakTrainerServiceLocation",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v13)
+        .iOS(.v15)
     ],
     products: [
-        .library(name: "WakTrainerServiceLocation", targets: ["WakTrainerServiceLocation"])
+        .library(
+            name: "WakTrainerServiceLocation",
+            targets: [
+                "WakTrainerServiceLocation"
+            ]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/iosdevbyul/WakTrainerCoreModels", branch: "main")
+        .package(
+            url: "https://github.com/iosdevbyul/WakTrainerCoreModels",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/iosdevbyul/TrisLocationKit",
+            branch: "main"
+        )
     ],
     targets: [
         .target(
             name: "WakTrainerServiceLocation",
             dependencies: [
-                .product(name: "WakTrainerCoreModels", package: "WakTrainerCoreModels")
+                .product(
+                    name: "WakTrainerCoreModels",
+                    package: "WakTrainerCoreModels"
+                ),
+                .product(
+                    name: "TrisLocationKit",
+                    package: "TrisLocationKit"
+                )
+            ]
+        ),
+        .testTarget(
+            name: "WakTrainerServiceLocationTests",
+            dependencies: [
+                "WakTrainerServiceLocation",
+                .product(
+                    name: "TrisLocationKit",
+                    package: "TrisLocationKit"
+                )
             ]
         )
     ]
